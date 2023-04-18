8687810c9948e23b43cc65cb11d4d5ad08fcbb25
Your Name <you@example.com>
2023-04-18 06:28:30 -0400

    m

--- Review-site/app.vue
+++ Review-site/app.vue
@@ -5,4 +5,10 @@
   <input type="text" placeholder="Search..">
 </div>
 
+<div>
+    <h2> home </h2>
+    <p> lorem </p>
+ </div>
+
+
 </template>
---
+++ Review-site/pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+ <div>
+    <h2> home </h2>
+    <p> lor</p>
+ </div>
+
+
+ </template>
+
+ <script setup>
+
+ </script>
+
+ <style scoped>
+
+ </style>
